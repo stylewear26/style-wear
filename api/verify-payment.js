e5adec07c0c111aff86adade138d3722d8ccdae4
stylewear26 <purushotamk180@gmail.com>
2026-09-24 14:14:39 +0530

    Update verify-payment.js

--- api/verify-payment.js
+++ api/verify-payment.js
@@ -49,7 +49,7 @@ module.exports = async function handler(req, res) {
     }
 
     return res.status(200).json({
-      success: true
+      success: true,
     message: "Payment verified successfully"
   });
 };
